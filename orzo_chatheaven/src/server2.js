068020dd22e15a9208ba1d2ac69f47510fa52f02
zekaran30 <zekaran30@gmail.com>
2025-02-06 00:26:12 -0500

    some code just went missing so adding it back

--- orzo_chatheaven/src/server2.js
+++ orzo_chatheaven/src/server2.js
@@ -10,6 +10,18 @@ const db = mysql.createConnection({
 
 app.use(cors()); 
 
+app.post('/login', (req, res)=>{
+    const sql = "SELECT * FROM login WHERE username = ? AND password = ?" ;
+    db.query(sql, [req.body.email, req.body.password], (err, data) =>{
+        if(err) return res.json("error"); 
+        if(data.length > 0) {
+            return res.json("Login successfull")
+        } else {
+            return res.json("User does not exist")
+        }
+    })
+})
+
 app.listen(8081, ()  =>{
     console.log("listening");
 })
