6728d33528535d691dabfaf5953fc9546b4c4198
zekaran30 <zekaran30@gmail.com>
2025-02-06 00:52:26 -0500

    works better

--- orzo_chatheaven/src/server2.js
+++ orzo_chatheaven/src/server2.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 
-const app = express(); 
+const app = express();
+app.use(cors()); 
+app.use(express.json()) 
 const db = mysql.createConnection({
     host: "localhost"
 
 })
 
-app.use(cors()); 
+app.post("/signup", (req, res) =>{
+    const {name,email, password} = req.body; 
+    console.log(name)
+})
 
 app.post('/login', (req, res)=>{
     const sql = "SELECT * FROM login WHERE username = ? AND password = ?" ;
