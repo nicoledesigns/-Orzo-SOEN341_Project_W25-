c0b2a3bf5a20f0d15b9eb16fa320453ea8414617
Eesha <Eeshajpatel@gmail.com>
2025-02-06 15:22:42 -0500

    Working version with backend

--- orzo_chatheaven/src/components/LoginSignup/LoginSignup.jsx
+++ orzo_chatheaven/src/components/LoginSignup/LoginSignup.jsx
@@ -1,148 +1,98 @@
-import React, { use, useState } from 'react'
-import './LoginSignup.css'
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/password.png'
-import axios from "axios"
-
+import React, { useState } from 'react';
+import './LoginSignup.css';
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
+import axios from 'axios';
 
 const LoginSignup = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [action, setAction] = useState('SignUp');
+    const [emailError, setEmailError] = useState(''); // Fixed emailError state
 
     const handleSignUp = (event) => {
         event.preventDefault(); // Prevent page refresh
         axios.post("http://localhost:8081/signup", { name, email, password })
-          .then(res => console.log("Signup Response:", res.data))
-          .catch(err => console.error("Signup Error:", err));
-          console.log("pipicaca")
-      };
-    function handleLogin(event){
+            .then(res => console.log("Signup Response:", res.data))
+            .catch(err => console.error("Signup Error:", err));
+    };
+
+    const handleLogin = (event) => {
         event.preventDefault(); 
-        axios.post('http://localhost:8081/login', {email, password})
-        .then (res => console.log(res))
-        .catch(err => console.log(err)); 
-        console.log("shit")
-    }
-  return (
-    <div className='container'>
-        <div className="bubbles">
-      <span style={{ "--i":11}}></span>
-      <span style={{ "--i":12 }}></span>
-      <span style={{ "--i":24 }}></span>
-      <span style={{ "--i":10 }}></span>
-      <span style={{ "--i":14 }}></span>
-      <span style={{ "--i":23 }}></span>
-      <span style={{ "--i":18 }}></span>
-      <span style={{ "--i":16 }}></span>
-      <span style={{ "--i":19 }}></span>
-      <span style={{ "--i":20 }}></span>
-      <span style={{ "--i":22 }}></span>
-      <span style={{ "--i":25 }}></span>
-      <span style={{ "--i":18 }}></span>
-      <span style={{ "--i":21 }}></span>
-      <span style={{ "--i":15 }}></span>
-      <span style={{ "--i":13 }}></span>
-      <span style={{ "--i":26 }}></span>
-      <span style={{ "--i":17 }}></span>
-      <span style={{ "--i":13 }}></span>
-      <span style={{ "--i":28 }}></span>
-      <span style={{ "--i":11}}></span>
-      <span style={{ "--i":12 }}></span>
-      <span style={{ "--i":24 }}></span>
-      <span style={{ "--i":10 }}></span>
-      <span style={{ "--i":14 }}></span>
-      <span style={{ "--i":23 }}></span>
-      <span style={{ "--i":18 }}></span>
-      <span style={{ "--i":16 }}></span>
-      <span style={{ "--i":19 }}></span>
-      <span style={{ "--i":20 }}></span>
-      <span style={{ "--i":22 }}></span>
-      <span style={{ "--i":25 }}></span>
-      <span style={{ "--i":18 }}></span>
-      <span style={{ "--i":21 }}></span>
-      <span style={{ "--i":15 }}></span>
-      <span style={{ "--i":13 }}></span>
-      <span style={{ "--i":26 }}></span>
-      <span style={{ "--i":17 }}></span>
-      <span style={{ "--i":13 }}></span>
-      <span style={{ "--i":28 }}></span>
-    </div>
-      <div className='header'>
-        <div className='text'>{action}</div>
-        <div className='underline'></div>
-      </div>
-      <div className='inputs'>
-        {action === 'Login' ? (
-          <div></div>
-        ) : (
-          <div className='input'>
-            <img src={user_icon} alt='' />
-            <input
-              type='text'
-              placeholder='Name'
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-        )}
-        <div className='input'>
-          <img src={email_icon} alt='' />
-          <input
-            type='email'
-            placeholder='E-mail'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          {/* Display Email Error */}
-          {emailError && <div className='error-message'>{emailError}</div>}
-        </div>
-        <div className='input'>
-          <img src={password_icon} alt='' />
-          <input
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className='inputs'>
-            {action==="Login"?<div></div>:
-            <div className='input'>
-            <img src={user_icon} alt=""/>
-            <input type='text'placeholder='Name'onChange={e => setName(e.target.value)}/>
-            </div>
-            }
-            <div className='input'>
-                <img src={email_icon} alt=""/>
-                <input type='email'placeholder='E-mail' onChange={e => setEmail(e.target.value)}/>
-            </div>
-            <div className='input'>
-                <img src={password_icon} alt=""/>
-                <input type='password' placeholder='Password' onChange={e => setPassword(e.target.value)}/>
-            </div>
+        axios.post('http://localhost:8081/login', { email, password })
+            .then(res => console.log("Login Response:", res.data))
+            .catch(err => console.log("Login Error:", err));
+    };
 
-            {action==="Sign Up"?<div></div>:
-            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
-            }
+    return (
+        <div className='container'>
+            <div className="bubbles">
+                {Array.from({ length: 40 }).map((_, index) => (
+                    <span key={index} style={{ "--i": (10 + (index % 19)) }}></span>
+                ))}
+            </div>
+            <div className='header'>
+                <div className='text'>{action}</div>
+                <div className='underline'></div>
+            </div>
+            <div className='inputs'>
+                {action === 'Login' ? null : (
+                    <div className='input'>
+                        <img src={user_icon} alt='' />
+                        <input
+                            type='text'
+                            placeholder='Name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                )}
+                <div className='input'>
+                    <img src={email_icon} alt='' />
+                    <input
+                        type='email'
+                        placeholder='E-mail'
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                            if (!e.target.value.includes('@')) {
+                                setEmailError('Invalid email format');
+                            } else {
+                                setEmailError('');
+                            }
+                        }}
+                    />
+                    {/* Display Email Error */}
+                    {emailError && <div className='error-message'>{emailError}</div>}
+                </div>
+                <div className='input'>
+                    <img src={password_icon} alt='' />
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
 
-            <div className='submit-container'>
-                <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-                <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
-                
-                <button className="submit"
-                    onClick={action === "Sign Up" ? handleSignUp : handleLogin}>
-                        submit
-                    
-                </button>
+                {action === "Sign Up" ? null : (
+                    <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
+                )}
 
+                <div className='submit-container'>
+                    <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => setAction("Sign Up")}>Sign Up</div>
+                    <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => setAction("Login")}>Login</div>
+
+                    <button className="submit"
+                        onClick={action === "Sign Up" ? handleSignUp : handleLogin}>
+                        Submit
+                    </button>
+                </div>
             </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default LoginSignup;
