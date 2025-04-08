30ce6f44d9311830adeee31bee5ff01244e32026
Karan Kumar <zekaran30@gmail.com>
2025-04-08 03:10:38 -0400

    dont leak the api key, i think

--- orzo_chatheaven/src/server2.js
+++ orzo_chatheaven/src/server2.js
@@ -10,7 +10,8 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 
 const app = express();
-const api_key = "AI";
+
+const api_key = process.env.gemini_key;
 console.log(`api key is ${api_key}`)
 const genAi = new GoogleGenerativeAI(api_key)
 const modelImage = genAi.getGenerativeModel({ model: "gemini-2.0-flash-exp-image-generation" });
