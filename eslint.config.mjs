0c4c861ef1cdcb0a9447045e6aeb11961dd049c3
Nicole <nicoleantounn@hotmail.com>
2025-04-11 20:25:12 -0400

    fix: add PropTypes for DirectMessaging component props (react/prop-types) ESLint rule: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/prop-types.md

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
