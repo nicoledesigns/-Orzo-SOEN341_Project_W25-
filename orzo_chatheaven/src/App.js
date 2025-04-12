79abf2339cad99982535058cc380f22f58011196
Nicole <nicoleantounn@hotmail.com>
2025-04-11 20:09:24 -0400

    fix: add missing React import for JSX usage (react/react-in-jsx-scope) ESLint rule: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md

--- orzo_chatheaven/src/App.js
+++ orzo_chatheaven/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginSignup from './components/LoginSignup/LoginSignup';
 import AdminDashboard from './components/AdminDashboard/AdminDashboard';
 import UserDashboard from './components/UserDashboard/UserDashboard';
+import React from 'react';
 
 function App() {
   return (
